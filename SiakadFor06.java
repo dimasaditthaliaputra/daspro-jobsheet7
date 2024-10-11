1612d562612b6698803addb2ce383ee88bd9ff13
dimasaditt <aditcraftyt@gmail.com>
2024-10-11 07:50:48 +0700

    modifikasi SiakadFor06

--- SiakadFor06.java
+++ SiakadFor06.java
@@ -1,9 +1,11 @@
 import java.util.Scanner;
+
 public class SiakadFor06 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         double nilai, tertinggi = 0, terendah = 100;
+        int lulus = 0, tidakLulus = 0;
 
         for (int i = 1; i <= 10; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-" + i + ": ");
@@ -14,9 +16,16 @@ public class SiakadFor06 {
             if (nilai < terendah) {
                 terendah = nilai;
             }
+            if (nilai >= 60) {
+                lulus++;
+            } else {
+                tidakLulus++;
+            }
         }
 
         System.out.println("Nilai tertinggi: " + tertinggi);
         System.out.println("Nilai terendah: " + terendah);
+        System.out.println("Jumlah siswa yang lulus: " + lulus);
+        System.out.println("Jumlah siswa yang tidak lulus: " + tidakLulus);
     }
 }
